4bf1a805d2c007458c62057ee1feeff767cad066
HarshilChavada04 <harshilchavada04092002@gmail.com>
2025-02-15 17:23:02 +0530

    updated project structure

--- model/data.model.js
+++ model/data.model.js
@@ -1,26 +1,26 @@
-const {db} = require("../config/db");
+const { db } = require("../config/db");
 
 function getAllUsers(callback) {
-  db.query("SELECT * FROM users",(err, results) => {
+  db.query("SELECT * FROM users", (err, results) => {
     if (err) return callback(err, null);
     return callback(null, results);
   });
 }
-function checkUserExists(username, email, callback) {
+function checkUserExists(objReqBodyData, callback) {
   db.query(
     "SELECT COUNT(*) as count FROM users WHERE username = ? AND email = ?",
-    [username, email],
+    [objReqBodyData.username, objReqBodyData.email],
     (err, results) => {
-      if (err)return callback(err, null);
+      if (err) return callback(err, null);
       return callback(null, results[0].count > 0);
     }
   );
 }
 
-function addNewUser(username, email, password, callback) {
+function addNewUser(objReqBodyData, callback) {
   db.query(
     "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-    [username, email, password],
+    [objReqBodyData.username, objReqBodyData.email, objReqBodyData.password],
     callback
   );
 }
--- server.js
+++ server.js
@@ -1,56 +1,16 @@
 const express = require("express");
 const mysql = require("mysql2");
-const { sendOTP, verifyOTP } = require("./controller/otpController");
-
-
 const app = express();
-const {
-  getAllUsers,
-  addNewUser,
-  checkUserExists,
-} = require("./model/data.model");
+const cors = require("cors");
+const { sendOTP, verifyOTP } = require("./controller/otpController");
+const userRouter = require("./routes/userRoutes.routes");
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  getAllUsers((err, users) => {
-    if (err) {
-      return res.status(500).json({ error: "Database error" });
-    }
-    return res.status(200).json(users);
-  });
-});
-
-app.post("/", (req, res) => {
-  let data = req.body;
-  if (!data.username || !data.email || !data.password) {
-    return res.status(400).json({ message: "Please enter all the details" });
-  }
-  checkUserExists(data.username, data.email, (err, exists) => {
-    if (err)
-      return res
-        .status(500)
-        .json({ error: "Database error due to exists check" });
-
-    if (exists) {
-      return res.status(400).json({ error: "User already exists" });
-    }
-
-    addNewUser(data.username, data.email, data.password, (err, newUser) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ error: "Database error due to adding new user" });
-      }
-      return res
-        .status(200)
-        .json({ newUser, message: "Data added successfully" });
-    });
-  });
-});
+app.use("/v1/api/", userRouter);
 
 app.post("/send-otp", sendOTP); // Send OTP API
-
 
 app.listen(3000, () => {
   console.log("Server started at port 3000");
